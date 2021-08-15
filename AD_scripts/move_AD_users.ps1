1d589b7d2964cba14dea71029f69c5f630a8b6d7
CRFullStack <hobdy.chris02@gmail.com>
2021-08-15 11:38:38 -0400

    Added move_AD_users script

---
+++ AD_scripts/move_AD_users.ps1
@@ -0,0 +1,12 @@
+﻿$OUpath = "OU=IT,OU=NewUsers,DC=CHS,DC=LOCAL"
+$OUtarget = "OU=Help Desk,OU=IT,OU=NewUsers,DC=CHS,DC=LOCAL"
+
+$UsersToMove = Get-ADUser -filter * -SearchBase $OUpath | select -first 5
+
+foreach($user in $UsersToMove){
+    Write-Host "Moving $($user.SamAccountName) to $OUtarget"
+    $userDN = (Get-ADUser -Identity $user.SamAccountName).DistinguishedName
+    Move-ADObject -Identity $userDN -TargetPath $OUtarget
+    Write-Host "Moved $($user.SamAccountName)"
+}
+write-host 'Done.'
