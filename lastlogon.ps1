c90a092ce1304e808ee592a41846e1af4a666252
ChrisH_ <hobdy.chris02@gmail.com>
2021-10-26 15:17:06 -0400

    find last login for list of users.

---
+++ lastlogon.ps1
@@ -0,0 +1,13 @@
+﻿function Get-LastLogon {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$File
+    )
+    $lou = gc -Path $File
+
+    foreach($usr in $lou){
+        get-ADuser -Identity $usr -Properties lastLogonDate | select SamAccountName, LastLogonDate, @{l="Exact Date"; e={((NEW-TIMESPAN –Start $_.LastLogonDate -End (Get-Date)) | select Days, Hours, Minutes)}} | Format-List    
+    }
+    
+}
